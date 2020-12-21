c8cf00a51e282442be75eb688b9b7df2b14f9eb2
Adrian Bondy <adrian.bondy@gmail.com>
2020-12-21 12:51:52 -0500

    12/21/2020 weekly update
    and added capability to plot symptomatic results and plot raw numbers instead of positivity rate

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Plots of the Princeton COVID testing program.
 Data obtained from: https://fall2020.princeton.edu/health-guidance/covid-19-dashboard
-![plot](plot.png)
+![plot](plot_Asymptomatic.png)
--- generate_plots.m
+++ generate_plots.m
@@ -1,12 +1,15 @@
-function generate_plots(varargin)
+function generate_plots(type,varargin)
     p=inputParser;
+    p.addRequired(type,@(x)validateattributes(x,{'char','string'},{'nonempty'}));
     p.addParameter('errorbar',false);
-    p.parse(varargin{:});
+    p.addParameter('raw_numbers',false);
+    p.parse(type,varargin{:});
+    type = validatestring(type,{'Asymptomatic','Symptomatic'},'generate_plots','type',1);
     params=p.Results;
     P=get_parameters;
     csv_files = dir(fullfile(P.repository_path,'data'));
     csv_files = {csv_files.name};
-    csv_files = csv_files(endsWith(csv_files,'.csv'));
+    csv_files = csv_files(endsWith(csv_files,[type,'.csv']));
     if numel(csv_files)<1
         warning('No CSV files found in %s.',fullfile(P.repository_path,'data'));
         return
@@ -31,7 +34,7 @@ function generate_plots(varargin)
             n_tests{i} = T{i}.Tests;
             n_pos{i} = T{i}.PositiveCases;
         end
-        if params.errorbar
+        if params.errorbar && ~params.raw_numbers
             [~,l,u] = bino_confidence(n_tests{i},n_pos{i});
             l(isnan(l))=0;
             h(i) = shadedErrorBar(datenum(dates{i}),100*n_pos{i}./n_tests{i},100*[u l]');hold on;
@@ -45,25 +48,40 @@ function generate_plots(varargin)
                 h(i).mainLine.LineStyle=':';
             end
         else
-            h(i) = plot(datenum(dates{i}),100*n_pos{i}./n_tests{i},'color',colors(i,:),'LineWidth',2,'Marker','.','MarkerSize',20);hold on;   
+            if params.raw_numbers
+                h(i) = plot(datenum(dates{i}),n_pos{i},'color',colors(i,:),'LineWidth',2,'Marker','.','MarkerSize',20);hold on;   
+            else
+                h(i) = plot(datenum(dates{i}),100*n_pos{i}./n_tests{i},'color',colors(i,:),'LineWidth',2,'Marker','.','MarkerSize',20);hold on;                   
+            end
             if i>length(csv_files)
                 h(i).LineStyle=':';
             end
         end
     end
-    if params.errorbar
+    if params.errorbar && ~params.raw_numbers
        legend([h.mainLine],names);
     else
        legend(h,names); 
     end
     set(gca,P.axes_properties{:});
     all_dates = datenum(unique([dates{:}]));
-    yl=get(gca,'ylim');
-    set(gca,'xlim',[min(all_dates) max(all_dates)],'xtick',all_dates(1:2:end),'ytick',[0:0.5:max(1,yl(2))],'ylim',[0 max(1,yl(2))]);
+    set(gca,'xlim',[min(all_dates) max(all_dates)],'xtick',all_dates(1:2:end));            
+    if ~params.raw_numbers
+        yl=get(gca,'ylim');          
+        switch type
+            case 'Asymptomatic'
+                set(gca,'ytick',[0:0.5:max(1,yl(2))],'ylim',[0 max(1,yl(2))]);
+            case 'Symptomatic'
+                set(gca,'ytick',[0:20:max(100,yl(2))],'ylim',[0 max(100,yl(2))]);            
+        end
+        ylabel('% Positivity Rate');        
+    else
+        ylabel('No. Positive Tests');                
+    end
     datetick('x', 'mmm-dd-yy','keeplimits','keepticks');    
-    ylabel('% Positivity Rate');
     xlabel('Week Ending');
+    grid on;
     for i=1:length(P.figure_image_format)
-        saveas(gcf,fullfile(P.repository_path,'plot'),P.figure_image_format{i});
+        saveas(gcf,fullfile(P.repository_path,['plot_',type]),P.figure_image_format{i});
     end
 end
